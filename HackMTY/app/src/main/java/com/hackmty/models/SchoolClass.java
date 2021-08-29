3d8fd83f4f64df8bd44c5c05e471db8f66194de3
CarlosCRG19 <carloscrg19@gmail.com>
2021-08-28 22:11:49 -0500

    Added main activity, user can see classes

--- HackMTY/app/src/main/java/com/hackmty/models/SchoolClass.java
+++ HackMTY/app/src/main/java/com/hackmty/models/SchoolClass.java
@@ -1,15 +1,27 @@
 package com.hackmty.models;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+
+import java.util.List;
 
 @ParseClassName("SchoolClass")
 public class SchoolClass extends ParseObject {
 
     public static final String KEY_CLASS_NAME = "classname";
+    public static final String KEY_PROFESSORS = "professors";
+    public static final String KEY_CODE = "code";
+    public static final String KEY_IMAGE = "classImage";
 
     public String getName(){
         return getString(KEY_CLASS_NAME);
     }
+
+    public List<String> getProfessors() { return getList(KEY_PROFESSORS); }
+
+    public String getCode() { return getString(KEY_CODE); }
+
+    public ParseFile getImage() { return (ParseFile) get("classImage"); }
 
 }
