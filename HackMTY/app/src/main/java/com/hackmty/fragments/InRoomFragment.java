31c9ee91b693bbfea26d4640e8f57aa9c83b4b5f
Carlo Angel Luján García <carlouxljlj@gmail.com>
2021-08-28 19:41:47 -0500

    Room model creation

--- HackMTY/app/src/main/java/com/hackmty/fragments/InRoomFragment.java
+++ HackMTY/app/src/main/java/com/hackmty/fragments/InRoomFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.davidmiguel.dragtoclose.DragToClose;
 import com.google.android.material.tabs.TabLayout;
 import com.hackmty.R;
 import com.hackmty.models.Room;
